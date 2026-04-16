3915562ade9014b8fdbddeebfea2e04ffef62d18
Team 9933 FTC <55024397+9933ftcrobotics@users.noreply.github.com>
2026-04-15 20:15:48 -0400

    Started shoot 3 ball command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoName.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoName.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import static android.os.SystemClock.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
@@ -24,19 +26,121 @@ public class AutoName extends TurtleOpMode {
 
 
             case 0:
-                drive.seedPose(0,0,0);
-                    step = 10;
+                drive.seedPose(0, 0, 0);
+                step = 10;
                 break;
             case 10:
-                drive.driveToPose(70,6,-40);
+                drive.driveToPose(70, 6, -40);
                 if (drive.isRobotAtTarget()) {
                     step = 20;
                 }
                 break;
             case 20:
-                shooter.setShooterSpeed(.5);
+                intake.start();
+                shooter.setShooterSpeed(.9);
+                sleep(3000);
                 step = 30;
                 break;
+            case 30:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 40;
+                break;
+            case 40:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 50;
+                break;
+            case 50:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 60;
+                break;
+            case 60:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 70;
+                break;
+            case 70:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 80;
+                break;
+            case 80:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 90;
+                break;
+            case 90:
+                drive.driveToPose(80,23,0);
+                if (drive.isRobotAtTarget()) {
+                    step = 100;
+                }
+                break;
+            case 100:
+                //sleep(4000);
+                intake.start();
+                sleep(1000);
+                step = 110;
+                break;
+            case 110:
+                drive.driveToPose(109,19,0);
+                if (drive.isRobotAtTarget()) {
+                    step = 120;
+                }
+                break;
+            case 120:
+                drive.driveToPose(70, 6, -40);
+                if (drive.isRobotAtTarget()) {
+                    step = 130;
+                }
+                break;
+            case 130:
+                intake.start();
+                shooter.setShooterSpeed(.9);
+                sleep(3000);
+                step = 140;
+                break;
+            case 140:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 150;
+                break;
+            case 150:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 160;
+                break;
+            case 160:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 170;
+                break;
+            case 170:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 180;
+                break;
+            case 180:
+                shooter.setLoading(.25);
+                delivery.setDeliverySpeed(1);
+                sleep(1200);
+                step = 190;
+                break;
+            case 190:
+                shooter.setLoading(0.075);
+                sleep(1200);
+                step = 300;
+                break;
+            case 300:
+                shooter.setShooterSpeed(0);
+                intake.stop();
+                delivery.setDeliverySpeed(0);
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubSystem.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static android.os.SystemClock.sleep;
+
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,6 +12,7 @@ public class ShooterSubSystem {
     Telemetry telemetry;
     DcMotorEx shooterMotor;
     Servo loading;
+    double step;
 
     public ShooterSubSystem(Telemetry telemetry, HardwareMap hardwareMap){
         this.telemetry = telemetry;
@@ -27,5 +30,47 @@ public class ShooterSubSystem {
 
         loading.setPosition(position);
     }
+    public boolean shootTres () {
+
+    //before running make sure the robot is positioned
+    // and the delivery + intake are running
+        switch (step) {
+
+            case 20:
+                setShooterSpeed(.9);
+                sleep(3000);
+                step = 30;
+                break;
+            case 30:
+                setLoading(.25);
+                sleep(1200);
+                step = 40;
+                break;
+            case 40:
+                setLoading(0.075);
+                sleep(1200);
+                step = 50;
+                break;
+            case 50:
+                setLoading(.25);
+                sleep(1200);
+                step = 60;
+                break;
+            case 60:
+                setLoading(0.075);
+                sleep(1200);
+                step = 70;
+                break;
+            case 70:
+                setLoading(.25);
+                sleep(1200);
+                step = 80;
+                break;
+            case 80:
+                setLoading(0.075);
+                sleep(1200);
+                return true;
+                break;
+        }
 
 }
