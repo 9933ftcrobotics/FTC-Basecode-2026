139cce71318a7d73afa1a25e2953e621cc1c18e0
9933 <9933ftcrobotics@gmail.com>
2026-04-22 20:26:17 -0400

    added sub system(not finished motr + servos )

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DeliverySubSystem.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DeliverySubSystem {
+    Telemetry telemetry;
+    DcMotorEx deliveryMotor;
+
+
+    public DeliverySubSystem(Telemetry telemetry, HardwareMap hardwareMap) {
+
+        this.telemetry=telemetry;
+            deliveryMotor = hardwareMap.get(DcMotorEx.class, "delivery");
+
+}
+        public void beltMove(double speed) {
+            deliveryMotor.setVelocity(speed)
+
+        }
+
+
+}
+
